b3b94c17f134cea6f3f7afdf8528affdb6d30962
Nils Hoffmann <the.nilshoffmann@gmail.com>
2019-03-02 19:19:41 +0100

    Refactored Navigation and added language switch

--- src/app/core/components/header/header.component.ts
+++ src/app/core/components/header/header.component.ts
@@ -1,14 +1,34 @@
 import { Component, OnInit } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
+import { Language } from '@app/core/models';
 
 @Component({
   selector: 'app-header', templateUrl: './header.component.html', styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() {
+  public languageDropdownActive: boolean = false;
+
+  constructor(public translate: TranslateService) {
   }
 
   ngOnInit() {
   }
 
+  toggleDropdown() {
+    this.languageDropdownActive = !this.languageDropdownActive;
+  }
+
+  getLangTranslation(currentLang: string) {
+    return this.translate.instant(Language.getByAlpha2Code(currentLang).translationKey);
+  }
+
+  public get languages(): Language[] {
+    return Language.keys();
+  }
+
+  useLanguage(lang: Language) {
+    this.translate.use(lang.alpha2Code);
+    this.languageDropdownActive = false;
+  }
 }
