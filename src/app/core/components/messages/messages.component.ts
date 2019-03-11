bdc6c8c3454e33b2cae23b1193e8e190593ae6e9
Nils Hoffmann <the.nilshoffmann@gmail.com>
2019-03-11 19:56:22 +0100

    Added Todos

--- src/app/core/components/messages/messages.component.ts
+++ src/app/core/components/messages/messages.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { MessageService } from '@app/core/services';
 
+// TODO: Remove from Module, unused by now
 @Component({
   selector:    'app-messages',
   templateUrl: './messages.component.html',
